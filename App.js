3f9999ff8b1c7078b53f8f9d9db184f3b9c3b99d
antvega <antvega627@gmail.com>
2023-12-04 22:28:44 -0800

    Simple working solution for rock paper scissors.

--- App.js
+++ App.js
@@ -2,13 +2,42 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
 import { useState } from "react";
 
+const MOVES = ["rock", "paper", "scissors"];
+
+function getRandomArrayElement(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
 export default function App() {
   const [playerChoice, setPlayerChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
+  const [outcome, setOutcome] = useState(null);
 
-  const makeMoves = (playerChoice) => {
-    setPlayerChoice(playerChoice);
-    setComputerChoice("rock");
+  const makeMoves = (pChoice) => {
+    setPlayerChoice(pChoice);
+    const cChoice = MOVES[getRandomArrayElement(MOVES)];
+    setComputerChoice(cChoice);
+    determineOutcome(pChoice, cChoice);
+  };
+
+  const determineOutcome = (playerChoice, computerChoice) => {
+    if (playerChoice === computerChoice) {
+      setOutcome("It's a tie!");
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+      setOutcome("You win!");
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+      setOutcome("You lose!");
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+      setOutcome("You lose!");
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+      setOutcome("You win!");
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+      setOutcome("You win!");
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+      setOutcome("You lose!");
+    } else {
+      console.log("Something went wrong.");
+    }
   };
 
   return (
@@ -16,8 +45,6 @@ export default function App() {
       style={{
         flex: 1,
         alignItems: "center",
-        borderWidth: 1,
-        borderColor: "red",
       }}
     >
       {playerChoice == null ? (
@@ -46,6 +73,7 @@ export default function App() {
         <>
           <Text>You picked {playerChoice}</Text>
           <Text>Computer picked {computerChoice}</Text>
+          <Text>{outcome}</Text>
           <Button
             title="reset"
             onPress={() => {
